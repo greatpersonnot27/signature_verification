487acf2f6a422150f84990e4a0615b86250512da
Irakli Vashakidze <greatpersonnot27@gmail.com>
2021-03-11 20:20:59 +0400

    Add basic DTW implementation #2

--- DTWbasic.py
+++ DTWbasic.py
@@ -1,1 +1,41 @@
-## TODO basic implementation of the DTW algorithm
+## Basic Dynamic time warping implementation
+import numpy as np
+
+
+def DTWbasic(s, t):
+    n, m = len(s), len(t)
+    dtw_matrix = np.zeros((n+1, m+1))
+    for i in range(n+1):
+        for j in range(m+1):
+            dtw_matrix[i, j] = np.inf
+
+    dtw_matrix[0, 0] = 0
+    
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            cost = abs(s[i-1] - t[j-1])
+            last_min = np.min([dtw_matrix[i-1, j], dtw_matrix[i, j-1], dtw_matrix[i-1, j-1]])
+            dtw_matrix[i, j] = cost + last_min
+    return dtw_matrix
+
+
+def DTWwindow(s, t, window):
+    n, m = len(s), len(t)
+    w = np.max([window, abs(n-m)])
+    dtw_matrix = np.zeros((n+1, m+1))
+    
+    for i in range(n+1):
+        for j in range(m+1):
+            dtw_matrix[i, j] = np.inf
+    dtw_matrix[0, 0] = 0
+    
+    for i in range(1, n+1):
+        for j in range(np.max([1, i-w]), np.min([m, i+w])+1):
+            dtw_matrix[i, j] = 0
+    
+    for i in range(1, n+1):
+        for j in range(np.max([1, i-w]), np.min([m, i+w])+1):
+            cost = abs(s[i-1] - t[j-1])
+            last_min = np.min([dtw_matrix[i-1, j], dtw_matrix[i, j-1], dtw_matrix[i-1, j-1]])
+            dtw_matrix[i, j] = cost + last_min
+    return dtw_matrix
