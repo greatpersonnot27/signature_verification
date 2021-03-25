2cac3f582872ca1e4f1be08a082d0ce2ddaf712c
Irakli Vashakidze <greatpersonnot27@gmail.com>
2021-03-26 00:08:38 +0400

    Add test data calculation

--- DTWbasic.py
+++ DTWbasic.py
@@ -1,51 +1,97 @@
 ## Basic Dynamic time warping implementation
 import numpy as np
 import math
+import itertools
 
-def DTWbasic(s, t):
-    n, m = len(s), len(t)
-    dtw_matrix = np.zeros((n+1, m+1))
-    for i in range(n+1):
-        for j in range(m+1):
-            dtw_matrix[i, j] = np.inf
+class DTWbasic():
+    def __init__(self, user_id, signatures):
+        self.signatures = signatures
+        self.training_data = [signature[1] for signature in signatures if int(signature[0]) <= 10]
+        self.test_data_real = [signature for signature in signatures if int(signature[0]) > 10 and int(signature[0]) < 21]
+        self.test_data_fake = [signature for signature in signatures if int(signature[0]) > 20]
+        self.user_id = user_id
+        self.threshhold = None
 
-    dtw_matrix[0, 0] = 0
     
-    for i in range(1, n+1):
-        for j in range(1, m+1):
-            cost = math.dist(s[i-1], t[j-1])
-            last_min = np.min([dtw_matrix[i-1, j], dtw_matrix[i, j-1], dtw_matrix[i-1, j-1]])
-            dtw_matrix[i, j] = cost + last_min
-    return dtw_matrix[n,m]
+    def get_DTWbasic(self, s, t):
+        n, m = len(s), len(t)
+        dtw_matrix = np.zeros((n+1, m+1))
+        for i in range(n+1):
+            for j in range(m+1):
+                dtw_matrix[i, j] = np.inf
+
+        dtw_matrix[0, 0] = 0
+        
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                cost = math.dist(s[i-1], t[j-1])
+                last_min = np.min([dtw_matrix[i-1, j], dtw_matrix[i, j-1], dtw_matrix[i-1, j-1]])
+                dtw_matrix[i, j] = cost + last_min
+        return dtw_matrix[n,m]
 
 
-def DTWwindow(s, t, window):
-    n, m = len(s), len(t)
-    w = np.max([window, abs(n-m)])
-    dtw_matrix = np.zeros((n+1, m+1))
-    
-    for i in range(n+1):
-        for j in range(m+1):
-            dtw_matrix[i, j] = np.inf
-    dtw_matrix[0, 0] = 0
-    
-    for i in range(1, n+1):
-        for j in range(np.max([1, i-w]), np.min([m, i+w])+1):
-            dtw_matrix[i, j] = 0
-    
-    for i in range(1, n+1):
-        for j in range(np.max([1, i-w]), np.min([m, i+w])+1):
-            cost = abs(s[i-1] - t[j-1])
-            last_min = np.min([dtw_matrix[i-1, j], dtw_matrix[i, j-1], dtw_matrix[i-1, j-1]])
-            dtw_matrix[i, j] = cost + last_min
-    return dtw_matrix
-
-def distance_euclidean():
-    pass
-
-def main():
-    pass
+    def get_DTWwindow(self, s, t, window):
+        n, m = len(s), len(t)
+        w = np.max([window, abs(n-m)])
+        dtw_matrix = np.zeros((n+1, m+1))
+        
+        for i in range(n+1):
+            for j in range(m+1):
+                dtw_matrix[i, j] = np.inf
+        dtw_matrix[0, 0] = 0
+        
+        for i in range(1, n+1):
+            for j in range(np.max([1, i-w]), np.min([m, i+w])+1):
+                dtw_matrix[i, j] = 0
+        
+        for i in range(1, n+1):
+            for j in range(np.max([1, i-w]), np.min([m, i+w])+1):
+                cost = abs(s[i-1] - t[j-1])
+                last_min = np.min([dtw_matrix[i-1, j], dtw_matrix[i, j-1], dtw_matrix[i-1, j-1]])
+                dtw_matrix[i, j] = cost + last_min
+        return dtw_matrix
 
 
-if __name__ == "__main__":
-    main()
+    def get_threshhold(self):
+        combs = list(itertools.combinations(self.training_data, 2))
+        diffs = []
+        for comb in combs:
+            s, t = comb
+            s = [[int(point['x-coordinate']),int(point['y-coordinate']), int(point['pressure'])] for point in s]
+            t = [[int(point['x-coordinate']),int(point['y-coordinate']), int(point['pressure'])] for point in t]
+            diffs.append(self.get_DTWbasic(s,t))
+        self.threshhold = sum(diffs)/ len(diffs)
+        return self.threshhold
+
+
+    def get_test_data_results(self, num_real = None, num_fake = None):
+        real_data, fake_data = self.test_data_real, self.test_data_fake
+        real_results = []
+        forgery_results = []
+        if num_real != None:
+            real_data = real_data[:num_real]
+        if num_fake != None:
+            fake_data = fake_data[:num_fake]
+        for test_signature in real_data:
+            test_key = test_signature[0]
+            test_values = test_signature[1]
+            test_diffs = []
+            for real_signature in self.training_data:
+                s = [[int(point['x-coordinate']),int(point['y-coordinate']), int(point['pressure'])] for point in real_signature]
+                t = [[int(point['x-coordinate']),int(point['y-coordinate']), int(point['pressure'])] for point in test_values]
+                test_diffs.append(self.get_DTWbasic(s,t))
+            test_ave_th = sum(test_diffs)/ len(test_diffs)
+            real_results.append(test_ave_th)
+        for test_signature in fake_data:
+            test_key = test_signature[0]
+            test_values = test_signature[1]
+            test_diffs = []
+            for real_signature in self.training_data:
+                s = [[int(point['x-coordinate']),int(point['y-coordinate']), int(point['pressure'])] for point in real_signature]
+                t = [[int(point['x-coordinate']),int(point['y-coordinate']), int(point['pressure'])] for point in test_values]
+                test_diffs.append(self.get_DTWbasic(s,t))
+            test_ave_th = sum(test_diffs)/ len(test_diffs)
+            forgery_results.append(test_ave_th)
+        return real_results, forgery_results
+
+        
--- loader.py
+++ loader.py
@@ -6,7 +6,7 @@ import csv
 import datetime
 import ast
 import easygui as gui
-import itertools
+
 
 from plotter import plot_signature, plot_scatter_signature
 from DTWbasic import DTWbasic
@@ -117,16 +117,6 @@ def get_user_signature_ids(filename):
         sign_id = id_checks.group(2)
     return user_id, sign_id
 
-def train_single_signature(real_signatures):
-    combs = list(itertools.combinations(real_signatures, 2))
-    diffs = []
-    for comb in combs:
-        s, t = comb
-        s = [[int(point['x-coordinate']),int(point['y-coordinate']), int(point['pressure'])] for point in s]
-        t = [[int(point['x-coordinate']),int(point['y-coordinate']), int(point['pressure'])] for point in t]
-        diffs.append(DTWbasic(s,t))
-    return sum(diffs)/ len(diffs)
-
 def main():
     data_folder = 'data_files'
     signatures = {}
@@ -138,15 +128,18 @@ def main():
             signatures[user_id].append([sign_id, single_signature_data])
         else:
             signatures[user_id] = [[sign_id, single_signature_data]]
-    signatures['1']
-    real_signatures = [signature[1] for signature in signatures['1'] if int(signature[0]) <= 10]
-    signature_th = train_single_signature(real_signatures)
-    # user_one_real = signatures['1'][0]
-    # user_one_real_x_values = [[int(point['x-coordinate']),int(point['y-coordinate']), int(point['pressure'])] for point in user_one_real[1]]
-    # user_one_fake = signatures['1'][13]
-    # user_one_fake_x_values = [[int(point['x-coordinate']),int(point['y-coordinate']), int(point['pressure'])] for point in user_one_fake[1]]
-    # #print(user_one_real_x_values, user_one_fake_x_values)
-    # print(DTWbasic(user_one_real_x_values, user_one_fake_x_values))
+    DTWlist = []
+    counter = 0
+    for key, signature_info in signatures.items():
+        dtwo = DTWbasic(key, signature_info)
+        threshhold = dtwo.get_threshhold()
+        DTWlist.append([threshhold, dtwo])
+        print(key)
+        r,f = dtwo.get_test_data_results(10,10)
+        if counter == 5:
+            break
+        counter += 1
+    print("DONE")
     # review_signatures(signatures)
     
 if __name__ == "__main__":
