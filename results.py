21d193cce851e6e158d3049a2a41512d6c4ec4f0
Irakli Vashakidze <greatpersonnot27@gmail.com>
2021-04-15 22:23:59 +0400

    Add save,load functions to results.py

--- results.py
+++ results.py
@@ -1,1 +1,28 @@
-# STORE verification results
+import json
+
+
+def save_results(results, features, classifier_name):
+    bank_dictionary = {'bookings': processors['bank'].get_bookings(
+    ), 'source_files': processors['bank'].source_files}
+    twint_dictionary = {'bookings': processors['twint'].get_bookings(
+    ), 'fee_bookings': processors['twint'].fee_bookings, 'source_files': processors['twint'].source_files}
+    payrex_dictionary = {'bookings': processors['payrexx'].get_bookings(
+    ), 'fee_bookings': processors['payrexx'].fee_bookings, 'source_files': processors['payrexx'].source_files}
+    paypal_dictionary = {'bookings': processors['paypal'].get_bookings(), 'fee_bookings': processors['paypal'].fee_bookings,
+                         'source_files': processors['paypal'].source_files, 'conversion_bookings': processors['paypal'].conversion_bookings}
+    with open("saved_state/saved_state.txt", "w") as fl:
+        json.dump({"bank_dict": bank_dictionary, "twint_dict": twint_dictionary, "payrex_dict": payrex_dictionary,
+                   "paypal_dict": paypal_dictionary, "source_files": source_files}, fl, indent=4)
+
+def load_results(name):
+    bank_dictionary = {'bookings': processors['bank'].get_bookings(
+    ), 'source_files': processors['bank'].source_files}
+    twint_dictionary = {'bookings': processors['twint'].get_bookings(
+    ), 'fee_bookings': processors['twint'].fee_bookings, 'source_files': processors['twint'].source_files}
+    payrex_dictionary = {'bookings': processors['payrexx'].get_bookings(
+    ), 'fee_bookings': processors['payrexx'].fee_bookings, 'source_files': processors['payrexx'].source_files}
+    paypal_dictionary = {'bookings': processors['paypal'].get_bookings(), 'fee_bookings': processors['paypal'].fee_bookings,
+                         'source_files': processors['paypal'].source_files, 'conversion_bookings': processors['paypal'].conversion_bookings}
+    with open("saved_state/saved_state.txt", "w") as fl:
+        json.dump({"bank_dict": bank_dictionary, "twint_dict": twint_dictionary, "payrex_dict": payrex_dictionary,
+                   "paypal_dict": paypal_dictionary, "source_files": source_files}, fl, indent=4)
